edfd2a9cf9140994b59ddc1ae22e44d65e1728ab
Alvaro Sanchez Duran <alvaro.sanchez@grupoica.com>
2019-12-19 12:43:05 +0100

    Modificaciones en los modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,20 +8,9 @@ import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 const appRoutes: Routes = [
-    { 
-        path: '', 
-        component: PagesComponent,
-        children: [
-            { path: 'dashboard', component: DashboardComponent },
-            { path: 'progress', component: ProgressComponent },
-            { path: 'graficas1', component: Graficas1Component },
-            { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-        ]
-    },
     { path: 'login', component: LoginComponent },
     { path: 'register', component:  RegisterComponent},
     { path: '**', component: NopagefoundComponent },
-
 ]
 
 export const APP_ROUTES = RouterModule.forRoot(appRoutes, {useHash: true});
